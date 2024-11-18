6673e6bfa5aed32b90d701b3ae3aa2f0b9e09b83
Alyaman Massarani <alyaman.maasarani@gmail.com>
2024-11-18 13:59:38 -0500

    implemented logout after 30 seconds of inactivity

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -1,19 +1,42 @@
+"use client";
 import type { Metadata } from "next";
 import "./globals.css";
+import { signOut } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { useIdleTimer } from "react-idle-timer";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { AuthProvider } from "@/context/auth-context";
 import Navbar from "@/components/Navbar";
 import { Box } from "@mui/material";
-
-export const metadata: Metadata = {
-  title: "Space",
-  description: "",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [state, setState] = useState<string>("Active");
+  const router = useRouter();
+
+  const onIdle = async () => {
+    setState("Idle");
+    await signOut(auth);
+    console.log("idle");
+    router.push("/");
+  };
+
+  const onActive = () => {
+    setState("Active");
+    console.log("active");
+  };
+
+  const { getRemainingTime } = useIdleTimer({
+    onIdle,
+    onActive,
+    timeout: 300_000,
+    throttle: 500,
+  });
+
   return (
     <html lang="en">
       <body>
